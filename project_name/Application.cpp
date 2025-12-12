37fd107c7c44a3aabf0ec4c876d83675b89e26b7
Hugo <abescat@insa-toulouse.fr>
2025-12-12 09:35:57 +0100

    Arduino LCD

--- project_name/Application.cpp
+++ project_name/Application.cpp
@@ -4,12 +4,17 @@
  * @brief Fichier source de l'application
  *********************************************************************/
 #include "Application.h"
+#include <SoftwareSerial.h>
 
+#include <Wire.h>
+#include "rgb_lcd.h"
+
+rgb_lcd lcd;
 
 Application::Application()
 {
-  // Code
-  ; 
+  Serial.begin(9600);
+  
 }
   
 Application::~Application()
@@ -20,13 +25,21 @@ Application::~Application()
 
 void Application::init(void)
 {
-  // Code
-    ;
+  Serial.println("Initialisation I2C pour LCD...");
+  
+  // TOUJOURS utiliser D2 et D1 pour I2C
+  Wire.begin(D2, D1); // <-- IMPORTANT !
+  delay(100);
+  
+  lcd.begin(16, 2);
+  lcd.setRGB(255, 0, 255);
+  lcd.clear();
+  lcd.print("Hello");
+
 }
 
 
 void Application::run(void)
 {
-  // Code
-    ;
+
 }
