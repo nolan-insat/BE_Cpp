9405f333de57b6688fc9456e6ec110b58b98711c
Nolan <reynier-nome@insa-toulouse.fr>
2026-01-09 08:22:51 +0100

    Wifi ajoute

---
+++ Smart_Home/Wifi.h
@@ -0,0 +1,31 @@
+#ifndef WIFI_H_
+#define WIFI_H_
+
+#include <iostream>
+#include "Actionneur.h"
+
+class Wifi : public Actionneur {
+protected:
+    string ssid;
+    string motdepasse;
+    bool connecte;
+
+public:
+    Wifi(int pin);
+    Wifi(int pin, string name);
+    
+    ~Wifi();
+    
+    void initWifi();
+    void deconnecter();
+    void connecter(string ssid, string motdepasse);
+    
+    string getSSID();
+    string getMotDePasse();
+    bool getConnecte();
+    
+    void setSSID(string ssid);
+    void setMotDePasse(string motdepasse);
+};
+
+#endif
